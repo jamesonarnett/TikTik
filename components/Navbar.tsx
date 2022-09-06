aae27825040b7a2519e10176c4c57f7005626ff5
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-05 20:53:49 -0500

    Navbar, back to work tomorrow

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,19 @@ import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import Logo from "../utils/tiktik-logo.png";
 
 export const Navbar = () => {
   return (
-    <div>
-      <Link href={""}>
-        <div>
-          <Image className="cursor-pointer" src="placeholder.png" />
+    <div className="w-full flex justify-between items-center border-b-2 border-grey-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="tiktik"
+            layout="responsive"
+          />
         </div>
       </Link>
     </div>
