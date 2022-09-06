e2bc2f102b6fc42cbd497cfae1735eb15ba27d31
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-06 17:31:06 -0500

    Setup for videoCard main component

--- utils/client.ts
+++ utils/client.ts
@@ -1,9 +1,9 @@
-import sanityClient from '@sanity/client';
+import sanityClient from "@sanity/client";
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
-  dataset: 'production',
-  apiVersion: '2022-03-10',
+  projectId: "4t4oo0k4",
+  dataset: "production",
+  apiVersion: "2022-09-06",
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
