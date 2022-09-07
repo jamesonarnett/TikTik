f97b521a96e64ae721ea0d51ed13d193eb3e9756
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-07 12:16:58 -0500

    Zustand persistant login, login functional

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AiOutlineLogout } from "react-icons/ai";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 export const Navbar = () => {
+  const { userProfile, addUser } = useAuthStore();
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-grey-200 py-2 px-4">
       <Link href="/">
@@ -20,6 +25,20 @@ export const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+      <div>
+        {userProfile ? (
+          <div>{userProfile.userName}</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => {
+              createOrGetUser(response, addUser);
+            }}
+            onError={(error: void) => console.log(error)}
+          />
+        )}
+      </div>
     </div>
   );
 };
