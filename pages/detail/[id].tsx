a4df85a359e6cd4eaede6ad4cda075189e5c4e28
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-08 12:38:29 -0500

    Setup for details page, UI fixes

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from "axios";
+import { BASE_URL } from "../../utils";
+import { Video } from "../../types";
 
-const Detail = () => {
-  return <div>Detail</div>;
+interface IProps {
+  postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  return <div></div>;
+};
+
+export const getServersideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: {
+      postDetails: data,
+    },
+  };
 };
 
 export default Detail;
